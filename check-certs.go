e730c6da7b2e2c6e9adefe474aa771ee90b3e2e8
有关部门 <t-kgg@163.com>
2025-04-11 10:20:53 +0800

    Update check-certs.go
    Multi msg  mergeing to one to send

--- check-certs.go
+++ check-certs.go
@@ -5,23 +5,25 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
-const defaultConcurrency = 8
 
 const (
-	errExpiringShortly = "%s: ** '%s' (S/N %X) expires in %d hours! **"
-	errExpiringSoon    = "%s: '%s' (S/N %X) expires in roughly %d days."
-	errSunsetAlg       = "%s: '%s' (S/N %X) expires after the sunset date for its signature algorithm '%s'."
+	errExpiringShortly = "%s: ** expires in %d hours! **"
+	errExpiringSoon    = "%s  expires in roughly %d days."
+	errSunsetAlg       = "%s  expires after the sunset date for its signature algorithm '%s'."
+	contentType = "application/json"
 )
 
 type sigAlgSunset struct {
@@ -36,23 +38,23 @@ type sigAlgSunset struct {
 // - https://technet.microsoft.com/en-us/library/security/2880823.aspx
 // - http://googleonlinesecurity.blogspot.com/2014/09/gradually-sunsetting-sha-1.html
 var sunsetSigAlgs = map[x509.SignatureAlgorithm]sigAlgSunset{
-	x509.MD2WithRSA: sigAlgSunset{
+	x509.MD2WithRSA: {
 		name:      "MD2 with RSA",
 		sunsetsAt: time.Now(),
 	},
-	x509.MD5WithRSA: sigAlgSunset{
+	x509.MD5WithRSA: {
 		name:      "MD5 with RSA",
 		sunsetsAt: time.Now(),
 	},
-	x509.SHA1WithRSA: sigAlgSunset{
+	x509.SHA1WithRSA: {
 		name:      "SHA1 with RSA",
 		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
 	},
-	x509.DSAWithSHA1: sigAlgSunset{
+	x509.DSAWithSHA1: {
 		name:      "DSA with SHA1",
 		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
 	},
-	x509.ECDSAWithSHA1: sigAlgSunset{
+	x509.ECDSAWithSHA1: {
 		name:      "ECDSA with SHA1",
 		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
 	},
@@ -64,23 +66,48 @@ var (
 	warnMonths  = flag.Int("months", 0, "Warn if the certificate will expire within this many months.")
 	warnDays    = flag.Int("days", 0, "Warn if the certificate will expire within this many days.")
 	checkSigAlg = flag.Bool("check-sig-alg", true, "Verify that non-root certificates are using a good signature algorithm.")
-	concurrency = flag.Int("concurrency", defaultConcurrency, "Maximum number of hosts to check at once.")
+	ddToken     = flag.String("token", "", "the url of dding")
 )
 
-type certErrors struct {
-	commonName string
-	errs       []error
+type TMessage struct {
+	MsgType string  `json:"msgtype"`
+	Text    Content `json:"text"`
+	At      At      `json:"at"`
+	IsAtAll bool    `json:"isAtAll"`
 }
 
-type hostResult struct {
-	host  string
-	err   error
-	certs []certErrors
+type Content struct {
+	Content string `json:"content"`
+}
+
+type At struct {
+	AtMobiles []string `json:"atMobiles"`
+}
+
+func NewTMessage(msg string, atMobiles []string, atAll bool) *TMessage {
+	if atMobiles == nil {
+		atMobiles = make([]string, 0)
+	}
+	atUsers := At{AtMobiles: atMobiles}
+	text := Content{Content: msg}
+	return &TMessage{
+		MsgType: "text",
+		Text:    text,
+		At:      atUsers,
+		IsAtAll: atAll,
+	}
+}
+
+func (tm *TMessage) Encode() []byte {
+	_bytes, err := json.Marshal(&tm)
+	if err != nil {
+		return nil
+	}
+	return _bytes
 }
 
 func main() {
 	flag.Parse()
-
 	if len(*hostsFile) == 0 {
 		flag.Usage()
 		return
@@ -97,126 +124,75 @@ func main() {
 	if *warnYears == 0 && *warnMonths == 0 && *warnDays == 0 {
 		*warnDays = 30
 	}
-	if *concurrency < 0 {
-		*concurrency = defaultConcurrency
+	if ddToken == nil || *ddToken == "" {
+		log.Println("dding token isn't set, notifying will be disable")
 	}
-
-	processHosts()
+	for {
+		processHosts()
+		log.Println("Sleeping 24 hours....")
+		time.Sleep(time.Hour * 24)
+	}
 }
 
 func processHosts() {
-	done := make(chan struct{})
-	defer close(done)
-
-	hosts := queueHosts(done)
-	results := make(chan hostResult)
-
-	var wg sync.WaitGroup
-	wg.Add(*concurrency)
-	for i := 0; i < *concurrency; i++ {
-		go func() {
-			processQueue(done, hosts, results)
-			wg.Done()
-		}()
-	}
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	for r := range results {
-		if r.err != nil {
-			log.Printf("%s: %v\n", r.host, r.err)
-			continue
-		}
-		for _, cert := range r.certs {
-			for _, err := range cert.errs {
-				log.Println(err)
-			}
-		}
-	}
-}
-
-func queueHosts(done <-chan struct{}) <-chan string {
-	hosts := make(chan string)
-	go func() {
-		defer close(hosts)
-
-		fileContents, err := ioutil.ReadFile(*hostsFile)
-		if err != nil {
-			return
-		}
-		lines := strings.Split(string(fileContents), "\n")
-		for _, line := range lines {
-			host := strings.TrimSpace(line)
-			if len(host) == 0 || host[0] == '#' {
-				continue
-			}
-			select {
-			case hosts <- host:
-			case <-done:
-				return
-			}
-		}
-	}()
-	return hosts
-}
-
-func processQueue(done <-chan struct{}, hosts <-chan string, results chan<- hostResult) {
-	for host := range hosts {
-		select {
-		case results <- checkHost(host):
-		case <-done:
-			return
-		}
-	}
-}
-
-func checkHost(host string) (result hostResult) {
-	result = hostResult{
-		host:  host,
-		certs: []certErrors{},
-	}
-	conn, err := tls.Dial("tcp", host, nil)
+	fileContents, err := os.ReadFile(*hostsFile)
 	if err != nil {
-		result.err = err
 		return
 	}
-	defer conn.Close()
-
-	timeNow := time.Now()
-	checkedCerts := make(map[string]struct{})
-	for _, chain := range conn.ConnectionState().VerifiedChains {
-		for certNum, cert := range chain {
-			if _, checked := checkedCerts[string(cert.Signature)]; checked {
+	lines := strings.Split(string(fileContents), "\n")
+	msgs := make([]string, 0)
+	for _, line := range lines {
+		host := strings.TrimSpace(line)
+		if len(host) == 0 || host[0] == '#' {
+			continue
+		}
+		msg := checkHost(host)
+		if msg != nil {
+			msg := strings.TrimSpace(*msg)
+			if msg == "" {
 				continue
 			}
-			checkedCerts[string(cert.Signature)] = struct{}{}
-			cErrs := []error{}
+			msgs = append(msgs, msg)
+		}
+	}
+	fullMsg := strings.Join(msgs, "\n")
+	log.Println("send notifying message\n", fullMsg)
+	if len(*ddToken) > 0 {
+		msg := NewTMessage(fullMsg, nil, false)
+		httpClient := http.Client{Timeout: time.Second * 5}
+		_, err := httpClient.Post(*ddToken, contentType, bytes.NewBuffer(msg.Encode()))
+		if err != nil {
+			log.Println(string(msg.Encode()), "发送到钉钉失败", err)
+		}
+	}
+}
 
+func checkHost(host string)  *string {
+	result := new(string)
+	conn, err := tls.Dial("tcp", host, nil)
+	if err != nil {
+		return nil
+	}
+	defer conn.Close()
+	timeNow := time.Now()
+	for _, chain := range conn.ConnectionState().VerifiedChains {
+		for certNum, cert := range chain {
 			// Check the expiration.
 			if timeNow.AddDate(*warnYears, *warnMonths, *warnDays).After(cert.NotAfter) {
 				expiresIn := int64(cert.NotAfter.Sub(timeNow).Hours())
 				if expiresIn <= 48 {
-					cErrs = append(cErrs, fmt.Errorf(errExpiringShortly, host, cert.Subject.CommonName, cert.SerialNumber, expiresIn))
+					*result = fmt.Sprintf(errExpiringShortly, host, expiresIn)
 				} else {
-					cErrs = append(cErrs, fmt.Errorf(errExpiringSoon, host, cert.Subject.CommonName, cert.SerialNumber, expiresIn/24))
+					*result = fmt.Sprintf(errExpiringSoon, host, expiresIn/24)
 				}
 			}
-
 			// Check the signature algorithm, ignoring the root certificate.
 			if alg, exists := sunsetSigAlgs[cert.SignatureAlgorithm]; *checkSigAlg && exists && certNum != len(chain)-1 {
 				if cert.NotAfter.Equal(alg.sunsetsAt) || cert.NotAfter.After(alg.sunsetsAt) {
-					cErrs = append(cErrs, fmt.Errorf(errSunsetAlg, host, cert.Subject.CommonName, cert.SerialNumber, alg.name))
+					*result =  fmt.Sprintf(errSunsetAlg, host, alg.name)
 				}
 			}
-
-			result.certs = append(result.certs, certErrors{
-				commonName: cert.Subject.CommonName,
-				errs:       cErrs,
-			})
 		}
 	}
-
-	return
+	return result
 }
